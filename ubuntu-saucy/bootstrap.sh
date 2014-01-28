6dc07234cb26f35037cc2c08e1fd4c649cfc6ec4
Neel Smith <dnsmith.neel@gmail.com>
2014-01-28 05:59:57 -0500

    Add saucy box.

---
+++ ubuntu-saucy/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+apt-get update
+# The one true editor
+apt-get install -y emacs
+
+# Web server
+apt-get install -y apache2
+rm -rf /var/www
+ln -fs /vagrant/web /var/www
+
+# version control
+apt-get install -y git
+
+# build system
+apt-get install -y gradle
+
+
+apt-get install -y nodejs
+apt-get install -y npm
+
+apt-get install -y ruby
+#gem install mdpress
+
