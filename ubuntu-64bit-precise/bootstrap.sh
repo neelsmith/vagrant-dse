e452e0e1e69d1f2bb33bf0c6ac5bf681086b506e
Neel Smith <dnsmith.neel@gmail.com>
2014-01-27 21:43:24 -0500

    Build gradle.

--- ubuntu-64bit-precise/bootstrap.sh
+++ ubuntu-64bit-precise/bootstrap.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
+# The one true editor
 apt-get install -y emacs
 
+# Web server
 apt-get install -y apache2
 rm -rf /var/www
 ln -fs /vagrant/web /var/www
+
+# build system
+apt-get install -y gradle
 
